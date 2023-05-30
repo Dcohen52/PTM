815aeb684d085bddd91d5b07ccffca61e80de905
Dekel Cohen <dcohen52@gmail.com>
2023-05-30 11:09:43 +0300

    Added get package info

--- ptm.py
+++ ptm.py
@@ -1,6 +1,7 @@
 ############################
 # ProtoCSS Package Manager #
 ############################
+import json
 
 import requests
 import os
@@ -35,26 +36,24 @@ class ProtoCSSPackageManager:
 
     def install_package(self, *packages, **kwargs):
         try:
-            for package_name in packages:
-                package_info = self._fetch_package_info(package_name)
-                if package_info is not None:
-                    print(f"Installing package '{package_name}'...")
-                    package_version = package_info["version"]
-                    package_file_name = f"{package_name}-{package_version}.ptm"
-                    package_file_path = os.path.join(self.package_dir, package_file_name)
-                    print(f"package_file_path: {package_file_path}")
-
-                    if os.path.exists(package_file_path):
-                        os.remove(package_file_path)
-
-                    self._download_file(package_info["download_url"], package_file_path)
+            for package in packages:
+                package_name = package
+                package_version = kwargs.get("version", "latest")
+                package_file_name = f"{package_name}-{package_version}.ptm"
+                package_file_path = os.path.join(self.package_dir, package_file_name)
+                if not os.path.exists(package_file_path):
+                    print(f"Downloading {package_name}...")
+                    r = requests.get(f"{self.server_url}/package/{package_name}/{package_version}")
+                    with open(package_file_path, "wb") as file:
+                        file.write(r.content)
+                    print(f"Installing {package_name}...")
                     shutil.unpack_archive(package_file_path, package_name)
                     os.remove(package_file_path)
-                    print(f"{Fore.GREEN}Package '{package_name}' installed successfully!{Style.RESET_ALL}")
+                    print(f"{package_name} installed successfully.")
                 else:
-                    print(f"{Fore.RED}Package '{package_name}' not found!{Style.RESET_ALL}")
+                    print(f"{package_name} is already installed.")
         except Exception as e:
-            print(f"{Fore.RED}Error: {e}{Style.RESET_ALL}")
+            print(f"{Fore.RED}Error: Package '{package_name}' not found.{Style.RESET_ALL}")
 
     def upload_package(self):
         try:
@@ -138,9 +137,13 @@ class ProtoCSSPackageManager:
                 # print(os.path.join(self.package_dir, f"{package_name}-{package_version}.zip"))
                 shutil.move(os.path.abspath(f"./{package_name}-{package_version}.zip"), package_file_path)
                 print(f"{Fore.YELLOW}New version moved.{Style.RESET_ALL}")
-                blob = storage.bucket().blob(f"packages/{package_file_name}")
+                # blob = storage.bucket().blob(f"packages/{package_name}/{package_version}/{package_file_name}")
+                # blob = storage.bucket().blob(f"packages/{package_name}/{package_version}/package.json")
                 print(f"{Fore.LIGHTYELLOW_EX}Uploading new version...{Style.RESET_ALL}")
-                blob.upload_from_filename(package_file_path)
+                for upload in [package_file_path, os.path.join(self.package_dir, "package.json")]:
+                    storage.bucket().blob(
+                        f"packages/{package_name}/{package_version}/{upload.split('/')[-1]}").upload_from_filename(
+                        upload)
                 print(
                     f"\n      {Back.LIGHTGREEN_EX}{Fore.LIGHTWHITE_EX}      Package '{package_name}' uploaded successfully!      {Style.RESET_ALL}\n")
 
@@ -156,11 +159,17 @@ class ProtoCSSPackageManager:
     def _fetch_package_info(self, package_name):
         print(f"Fetching package '{package_name}'...")
         try:
-            response = requests.get(f"https://{__storage_bucket__}/packages/{package_name}-1.0.0.ptm")
-            if response.status_code == 200:
-                return response.json()
-            else:
-                return None
+            # Authenticate the user with appropriate credentials
+            # cred = credentials.Certificate(__cert_path__)
+            # firebase_admin.initialize_app(cred, {'storageBucket': __storage_bucket__})
+
+            # Access the package.json file from Firebase Storage
+            bucket = storage.bucket()
+            blob = bucket.blob(f"packages/{package_name}/1_0_0_/package.json")
+            package_info = blob.download_as_text()
+            package_info = json.loads(package_info)
+
+            return package_info
         except Exception as e:
             print(f"{Fore.RED}Error: {e}{Style.RESET_ALL}")
 
@@ -181,7 +190,7 @@ def main():
     parser.add_argument("-i", "--install", action="store_true", help="Install packages")
     parser.add_argument("-u", "--upgrade", action="store_true", help="Upgrade packages")
     parser.add_argument("-gi", "--get-info", action="store_true", help="Get package information")
-    parser.add_argument("-up", "--upload", action="store_true", help="Upload package to firebase")
+    parser.add_argument("-up", "--upload", action="store_true", help="Upload a package")
 
     args = parser.parse_args()
 
@@ -197,11 +206,10 @@ def main():
         for package_name in args.packages:
             package_info = ptm.get_package_info(package_name)
             if package_info is not None:
-                print(f"Package: {package_name}")
-                print(f"Description: {package_info.get('description')}")
-                print(f"Author: {package_info.get('author')}")
-                print(f"Version: {package_info.get('version')}")
-                print(f"Files: {package_info.get('files')}")
+                print(f"{Fore.LIGHTWHITE_EX}Package name:{Style.RESET_ALL} {package_name}")
+                print(f"{Fore.LIGHTWHITE_EX}Version:{Style.RESET_ALL} {package_info['version'].replace('_', '.')}")
+                print(f"{Fore.LIGHTWHITE_EX}Author:{Style.RESET_ALL} {package_info['author']}")
+                print(f"{Fore.LIGHTWHITE_EX}Description:{Style.RESET_ALL} {package_info['description']}")
                 print()
             else:
                 print(f"Package '{package_name}' not found.")
@@ -215,3 +223,16 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# TODO:
+#  - Add support for installing packages
+#  - Add support for upgrading packages
+#  - Add support for getting package information - CHECKED
+#  - Add support for uploading packages: - CHECKED
+#       - Upload with existing package.json - CHECKED
+#       - Upload without existing package.json - CHECKED
+#  - Add login mechanism:
+#       - Ability to get the user's packages within the website.
+#       - Ability to upload packages to the user's account (the only way to upload packages).
+#       - Ability to install packages from the user's account, and track history of installed packages.
+#  - Add support for private packages.
